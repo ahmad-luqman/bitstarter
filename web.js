66c9883df076740d4ac395fb9815b3adee4e969c
Ahmad Luqman <ahmad.luqman@gmail.com>
2013-07-07 13:46:07 +0000

    Another attempt to fix buffer toString bug

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-  var str = fs.readFileSync("index.html");
+  var buf = new Buffer (fs.readFileSync("index.html"));
+  var str = buf.toString();
   response.send(str);
 });
 
